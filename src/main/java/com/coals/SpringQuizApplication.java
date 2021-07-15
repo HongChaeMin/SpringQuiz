7523b4bc1db3eb2158273a283b4b3777887592d2
DESKTOP-CQH0EH6\ghdco <ghdcoalss33@gmail.com>
2021-07-15 17:52:13 +0900

    수정

--- src/main/java/com/coals/SpringQuizApplication.java
+++ src/main/java/com/coals/SpringQuizApplication.java
@@ -1,9 +1,16 @@
 package com.coals;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @MapperScan(basePackages = "com.coals.*")
 @ComponentScan(basePackages = "com.*")
@@ -14,4 +21,15 @@ public class SpringQuizApplication {
 		SpringApplication.run(SpringQuizApplication.class, args);
 	}
 
+	@Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
+	
 }
--- src/main/java/com/coals/lesson02/StoreRestController.java
+++ src/main/java/com/coals/lesson02/StoreRestController.java
@@ -14,7 +14,7 @@ public class StoreRestController {
 	@Autowired
 	private StoreBO storeBO;
 	
-	@RequestMapping("lesson02/quiz01")
+	@RequestMapping("/lesson02/quiz01")
 	public List<com.coals.lesson02.model.Store> printStore() {
 		List<com.coals.lesson02.model.Store> list = storeBO.getStoreList();
 		return list;
