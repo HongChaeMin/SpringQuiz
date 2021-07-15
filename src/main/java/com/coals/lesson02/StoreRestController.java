e13c2cef435b73dd05cf6eb88c81f7b5c9c0a763
DESKTOP-CQH0EH6\ghdco <ghdcoalss33@gmail.com>
2021-07-15 16:28:03 +0900

    mybatis

---
+++ src/main/java/com/coals/lesson02/StoreRestController.java
@@ -0,0 +1,23 @@
+package com.coals.lesson02;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coals.lesson02.bo.StoreBO;
+
+@RestController
+public class StoreRestController {
+
+	@Autowired
+	private StoreBO storeBO;
+	
+	@RequestMapping("lesson02/quiz01")
+	public List<com.coals.lesson02.model.Store> printStore() {
+		List<com.coals.lesson02.model.Store> list = storeBO.getStoreList();
+		return list;
+	}
+	
+}
---
+++ src/main/java/com/coals/lesson02/bo/StoreBO.java
@@ -0,0 +1,21 @@
+package com.coals.lesson02.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coals.lesson02.dao.StoreDAO;
+import com.coals.lesson02.model.Store;
+
+@Service
+public class StoreBO {
+
+	@Autowired
+	private StoreDAO storeDAO;
+	
+	public List<Store> getStoreList() {
+		return storeDAO.selectStoreList();
+	}
+	
+}
